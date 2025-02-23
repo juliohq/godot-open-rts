41278c9a1ce6471dacb7b9ce154462d11dc5165d
juliohq <juliohenrique31501234@gmail.com>
2025-02-23 10:40:37 -0300

    Add ability to skip logos

--- source/Logos.gd
+++ source/Logos.gd
@@ -9,3 +9,13 @@ func _ready():
 		return
 	_animation_player.animation_finished.connect(func(_animation_name): queue_free())
 	_animation_player.play("animate_logos")
+
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventKey or event is InputEventMouseButton) and event.is_pressed():
+		get_viewport().set_input_as_handled()
+		
+		if _animation_player.current_animation_position < 1.5:
+			_animation_player.seek(1.5)
+		else:
+			queue_free()
